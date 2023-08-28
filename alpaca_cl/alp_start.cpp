8f63478fda79be5b33f112d28c747ca9fd8d1712
Alex Pavy <alex.g.pavy@gmail.com>
2023-08-28 09:47:41 +0100

    alpaca: move dir

--- alpaca_cl/alp_start.cpp
+++ alpaca_cl/alp_start.cpp
@@ -1,19 +1,15 @@
 #include <vector>
 #include <string>
-
-
 #include <iostream>
-
 #include "alpaca/client.h"
 #include "alpaca/config.h"
-
 #include "alp_start.h"
 #include "alp_main.h"
 
 using namespace std;
 
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     // Parse the required environment variables using the supplied helper utility
     auto env = alpaca::Environment();
     if (auto status = env.parse(); !status.ok()) {
